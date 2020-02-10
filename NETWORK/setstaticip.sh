96c335e6feb3a2b256d72d315b607d2dd4b82a4e
icebowl <icebowl1967@gmail.com>
2020-02-10 09:46:30 -0800

    AA

--- NETWORK/setstaticip.sh
+++ NETWORK/setstaticip.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-ifconfig wlan0 192.168.7.???? netmask 255.255.255.0;
+# Run this as a root user (sudo -i on a rpi)
+dhclient wlan0;
+ifconfig wlan0 192.168.7.??? netmask 255.255.255.0;
 rm -f /etc/resolv.conf;
-touch /etc/resolve.conf;
+touch /etc/resolv.conf;
 echo "nameserver 192.168.7.1" > /etc/resolv.conf;
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf;
+echo "nameserver 10.40.0.14" >> /etc/resolv.conf;
 
